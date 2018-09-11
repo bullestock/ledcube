92aa396a4a80010aab0c202c3c479f92e1cb10e4
Torsten Martinsen <torsten@bullestock.net>
2018-09-11 22:27:48 +0200

    Adapt for cube layout.

--- esp32/fire.cpp
+++ esp32/fire.cpp
@@ -60,17 +60,24 @@ public:
 
         // Step 1.  Cool down every cell a little
         for (int i = 0; i < NUM_LEDS; i++)
-            heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+            heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / NUM_LEDS_PER_STRAND) + 2));
   
         // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-        for (int k = NUM_LEDS - 1; k >= 2; k--)
-            heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
-    
-        // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-        if (random8() < SPARKING)
+        for (int s = 0; s < NUM_OF_STRANDS; ++s)
         {
-            int y = random8(7);
-            heat[y] = qadd8(heat[y], random8(160, 255));
+            const int offset = s * NUM_LEDS_PER_STRAND;
+            for (int k = NUM_LEDS_PER_STRAND - 1; k >= 2; k--)
+                heat[offset + k] = (heat[offset + k - 1] + heat[offset + k - 2] + heat[offset + k - 2]) / 3;
+        }
+        // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+        for (int s = 0; s < NUM_OF_STRANDS; ++s)
+        {
+            const int offset = s * NUM_LEDS_PER_STRAND;
+            if (random8() < SPARKING)
+            {
+                int y = random8(7);
+                heat[offset + y] = qadd8(heat[offset + y], random8(160, 255));
+            }
         }
 
         // Step 4.  Map from heat cells to LED colors
