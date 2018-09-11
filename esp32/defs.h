0775f3a25d85d405a74379c8f441c2cf9588ed8b
Torsten Martinsen <torsten@bullestock.net>
2018-09-11 22:28:36 +0200

    Add BRIGHTNESS.

--- esp32/defs.h
+++ esp32/defs.h
@@ -1,3 +1,4 @@
 const int NUM_LEDS_PER_STRAND = 8*8;
-const int NUM_OF_STRANDS = 1; //8;
+const int NUM_OF_STRANDS = 8;
 const int NUM_LEDS = NUM_OF_STRANDS * NUM_LEDS_PER_STRAND;
+const int BRIGHTNESS = 10; // percent
