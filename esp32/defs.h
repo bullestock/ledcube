19bde043c67b9a2bcce645dd2b79366a07f48034
Torsten Martinsen <torsten@bullestock.net>
2018-09-11 22:43:06 +0200

    Add more definitions.

--- esp32/defs.h
+++ esp32/defs.h
@@ -1,4 +1,6 @@
-const int NUM_LEDS_PER_STRAND = 8*8;
+const int NUM_LEDS_PER_ROW = 8;
+const int NUM_ROWS_PER_STRAND = 8;
+const int NUM_LEDS_PER_STRAND = NUM_LEDS_PER_ROW * NUM_ROWS_PER_STRAND;
 const int NUM_OF_STRANDS = 8;
 const int NUM_LEDS = NUM_OF_STRANDS * NUM_LEDS_PER_STRAND;
 const int BRIGHTNESS = 10; // percent
