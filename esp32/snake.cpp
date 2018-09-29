d453b2ec1102171c4b96750cc7830f27112445e9
Torsten Martinsen <torsten@bullestock.net>
2018-09-29 21:32:59 +0200

    Try not to cross other snakes.

--- esp32/snake.cpp
+++ esp32/snake.cpp
@@ -8,110 +8,64 @@ class Snake : public Program
 {
 public:
     Snake()
-        : Program(2)
+        : Program(5)
     {
     }
 
     static const int NOF_SNAKES = 5;
 
-    enum Direction {
-        DIR_UP,
-        DIR_LEFT,
-        DIR_FRONT,
-        DIR_RIGHT,
-        DIR_BACK,
-        DIR_DOWN,
-        DIR_SIZE
-    };
-    
     bool run() override;
 
 private:
     struct SnakeData
     {
         int age = 0;
-        int x = 0;
-        int y = 0;
-        int z = 0;
+        Position pos;
         Direction dir = DIR_UP;
         pixelColor_t c;
     };
 
-    bool limit_hit_imminent(Direction dir, int x, int y, int z)
+    bool limit_hit_imminent(Direction dir, const Position& pos)
     {
-        advance(dir, x, y, z);
-        return
-           (x < 0) ||
-           (x >= NUM_LEDS_PER_ROW) ||
-           (y < 0) ||
-           (y >= NUM_LEDS_PER_ROW) ||
-           (z < 0) ||
-           (z >= NUM_LEDS_PER_ROW);
+        auto new_pos = pos;
+        advance(dir, new_pos);
+        if ((new_pos.x < 0) ||
+            (new_pos.x >= NUM_LEDS_PER_ROW) ||
+            (new_pos.y < 0) ||
+            (new_pos.y >= NUM_LEDS_PER_ROW) ||
+            (new_pos.z < 0) ||
+            (new_pos.z >= NUM_LEDS_PER_ROW))
+            return true;
+        const auto p = get_pixel(new_pos);
+        //Serial.println(p.r + p.g + p.b);
+        return p.r + p.g + p.b > 50;
     }
 
-    void advance(Direction dir, int& x, int& y, int& z)
+    void advance(Direction dir, Position& pos)
     {
         switch (dir)
         {
         case DIR_UP:
-            ++z;
+            ++pos.z;
             break;
         case DIR_LEFT:
-            --x;
+            --pos.x;
             break;
         case DIR_FRONT:
-            ++y;
+            ++pos.y;
             break;
         case DIR_RIGHT:
-            ++x;
+            ++pos.x;
             break;
         case DIR_BACK:
-            --y;
+            --pos.y;
             break;
         case DIR_DOWN:
-            --z;
+            --pos.z;
             break;
         default:
             internal_error();
             break;
-        }
-    }
-
-    void change_direction(Direction& dir)
-    {
-        while (1)
-        {
-            const auto new_dir = (Direction) random8(0, DIR_SIZE);
-            bool ok = false;
-            switch (dir)
-            {
-            case DIR_UP:
-                ok = (new_dir != DIR_DOWN);
-                break;
-            case DIR_LEFT:
-                ok = (new_dir != DIR_RIGHT);
-                break;
-            case DIR_FRONT:
-                ok = (new_dir != DIR_BACK);
-                break;
-            case DIR_RIGHT:
-                ok = (new_dir != DIR_RIGHT);
-                break;
-            case DIR_BACK:
-                ok = (new_dir != DIR_FRONT);
-                break;
-            case DIR_DOWN:
-                ok = (new_dir != DIR_UP);
-                break;
-            default:
-                internal_error();
-                break;
-            }
-            if (ok)
-            {
-                dir = new_dir;
-                return;
-            }
         }
     }
 
@@ -123,37 +77,68 @@ bool Snake::run()
 {
     if (limiter.skip()) return false;
 
+    int idx = 0;
     for (auto& s : snakes)
     {
+        ++idx;
         // Randomly kill old snakes
         if (s.age > 50 + random8(0, 100))
             s.age = 0;
         if (s.age == 0)
         {
             // Initialize: Choose random start position and direction
-            s.x = random8(0, NUM_LEDS_PER_ROW);
-            s.y = random8(0, NUM_LEDS_PER_ROW);
-            s.z = random8(0, NUM_LEDS_PER_ROW);
-            s.dir = (Direction) random8(0, DIR_SIZE);
+            s.pos = random_position();
+            s.dir = random_direction();
             s.c = cc.get();
             cc.next();
-            set_pixel(s.x, s.y, s.z, s.c);
+            set_pixel(s.pos, s.c);
         }
         else
         {
             // Update
             if (random8(0, 10) < 4)
                 change_direction(s.dir);
-            
-            while (limit_hit_imminent(s.dir, s.x, s.y, s.z))
-                change_direction(s.dir);
 
-            advance(s.dir, s.x, s.y, s.z);
-            set_pixel(s.x, s.y, s.z, s.c);
+            int tries = 0;
+            const auto old_dir = s.dir;
+            while (limit_hit_imminent(s.dir, s.pos))
+            {
+                if (++tries > 10)
+                {
+                    Serial.print("Give up on ");
+                    Serial.println(idx);
+                    s.age = 0;
+                    fade_all(20);
+                    return true;
+                }
+                int dir_tries = 0;
+                do
+                {
+                    change_direction(s.dir);
+                    if (++dir_tries > 20)
+                    {
+                        Serial.print("Cannot find opposite of ");
+                        Serial.print(old_dir);
+                        Serial.print(" at ");
+                        Serial.print(s.pos.x);
+                        Serial.print(", ");
+                        Serial.print(s.pos.y);
+                        Serial.print(", ");
+                        Serial.println(s.pos.z);
+                        internal_error();
+                    }
+                }
+                while (is_opposite(s.dir, old_dir));
+            }
+            if (s.age > 0)
+            {
+                advance(s.dir, s.pos);
+                set_pixel(s.pos, s.c);
+            }
         }
         ++s.age;
     }
-    fade_all(10);
+    fade_all(20);
     return true;
 }
 
