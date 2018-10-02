4bdf12f8e36fbcce16730e9694e43720fcd6313d
Torsten Martinsen <torsten@bullestock.net>
2018-10-02 20:33:18 +0200

    Fix extent of even-size blocks.

--- esp32/block.cpp
+++ esp32/block.cpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include "block.h"
 
 Block::Block(pixelColor_t colour,
@@ -18,6 +19,34 @@ void Block::show(pixelColor_t c)
 {
     Position p1, p2;
     compute_extent(p1, p2);
+    if (!is_inside(p1) || !is_inside(p2))
+    {
+        Serial.print("Block size ");
+        Serial.print(size.x);
+        Serial.print(", ");
+        Serial.print(size.y);
+        Serial.print(", ");
+        Serial.print(size.z);
+        Serial.print(" at ");
+        Serial.print(pos.x);
+        Serial.print(", ");
+        Serial.print(pos.y);
+        Serial.print(", ");
+        Serial.print(pos.z);
+        Serial.print(": show: p1 ");
+        Serial.print(p1.x);
+        Serial.print(", ");
+        Serial.print(p1.y);
+        Serial.print(", ");
+        Serial.print(p1.z);
+        Serial.print(": show: p2 ");
+        Serial.print(p2.x);
+        Serial.print(", ");
+        Serial.print(p2.y);
+        Serial.print(", ");
+        Serial.println(p2.z);
+        internal_error();
+    }
     for (int x = p1.x; x <= p2.x; ++x)
         for (int y = p1.y; y <= p2.y; ++y)
             for (int z = p1.z; z <= p2.z; ++z)
@@ -48,6 +77,16 @@ Position Block::position() const
     return pos;
 }
 
+void Block::set_position(const Position& p)
+{
+    pos = p;
+}
+
+void Block::set_size(const Position& p)
+{
+    size = p;
+}
+
 void Block::compute_extent(const Position& p,
                            Position& p1,
                            Position& p2) const
@@ -55,9 +94,9 @@ void Block::compute_extent(const Position& p,
     p1.x = p.x - size.x/2;
     p1.y = p.y - size.y/2;
     p1.z = p.z - size.z/2;
-    p2.x = p.x + size.x/2;
-    p2.y = p.y + size.y/2;
-    p2.z = p.z + size.z/2;
+    p2.x = p.x + size.x/2 - (size.x % 2 ? 0 : 1);
+    p2.y = p.y + size.y/2 - (size.y % 2 ? 0 : 1);
+    p2.z = p.z + size.z/2 - (size.z % 2 ? 0 : 1);
 }
 
 void Block::compute_extent(Position& p1,
